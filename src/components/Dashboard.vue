142e8228cfbb4af769e09bf07592f7b6f006eb40
Inffinite <louislaizer4@gmail.com>
2020-08-25 01:16:32 +0300

    Minor ui changes

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -29,6 +29,17 @@
                             </div>
                         </div>
                     </div>
+
+                    <div class="profile-info-wrapper">
+                        <div class="profile-info">
+                            <div class="profile-info-name">
+                                Files (3)
+                            </div>
+                            <div class="profile-info-icon">
+                                <svg class="down-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M4.516 7.548c.436-.446 1.043-.481 1.576 0L10 11.295l3.908-3.747c.533-.481 1.141-.446 1.574 0 .436.445.408 1.197 0 1.615-.406.418-4.695 4.502-4.695 4.502a1.095 1.095 0 01-1.576 0S4.924 9.581 4.516 9.163c-.409-.418-.436-1.17 0-1.615z"/></svg>
+                            </div>
+                        </div>
+                    </div>
             </div>
         </div>
     </div>
